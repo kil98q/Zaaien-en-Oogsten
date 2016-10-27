fbdfe6f3fa5c4cdab745b95e2a0730453021f00c
kil98q <killiandekok@gmail.com>
2016-10-27 10:14:40 +0200

    Half finished scripts

---
+++ Assets/_Scripts/Harvest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Harvest : MonoBehaviour {
+    Vector3 _cursorWorld; // Will be changed when the input handler is ready
+    Collider2D Collisionbox;
+
+	// Update is called once per frame
+	void Update () {
+        Collisionbox = GetComponent<Collider2D>();
+	    _cursorWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Collisionbox.)
+        {
+
+        }
+	}
+}
---
+++ Assets/_Scripts/land.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class land : MonoBehaviour {
+    [SerializeField]
+    public int Water
+    {
+        get { return Water; }
+        private set { this.Water = value; }
+    }
+    [SerializeField]
+    public GameObject Plant
+    {
+        get { return Plant; }
+        private set { this.Plant = value; }
+    }
+    [SerializeField]
+    public bool HasPlants
+    {
+        get { return HasPlants; }
+        private set { this.HasPlants = value; }
+    }
+    public int MaxWater
+    {
+        get { return MaxWater; }
+        private set { this.MaxWater = value; }
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    public void AddWater(int Amount)
+    {
+        int newWater = Amount + Water;
+        if(newWater > 100)
+        {
+            Water = MaxWater;
+        }
+        
+    }
+    public void CalculateWater()
+    {
+
+    }
+}
