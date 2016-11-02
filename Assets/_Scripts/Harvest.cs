a8c7457877a8762227a30859aa9c9f9268858ed3
NabilCherigui <mrtopgamer@hotmail.com>
2016-11-02 15:51:26 +0100

    De beste commit. Iedereen weet het, jij weet, ik weet het.

---
+++ Assets/_Scripts/Harvest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Harvest : MonoBehaviour {
+    Vector3 _cursorWorld; // Will be changed when the input handler is ready
+    Collider2D Collisionbox;
+<<<<<<< HEAD
+
+	// Update is called once per frame
+	void Update () {
+        Collisionbox = GetComponent<Collider2D>();
+	    _cursorWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //if (Collisionbox.)
+        {
+
+        }
+	}
+=======
+    
+    Ray ray;
+    
+	// Update is called once per frame
+	void Update () {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+
+    }
+    private void PlayAnimation()
+    {
+
+    }
+    private Item[] HarvestLand(GameObject land)
+    {
+        return land.GetComponent<land>().RemovePlant();
+    }
+>>>>>>> f70e1c4a49b2e5112d9287efe0a5a65bf3c2cf24
+}
---
+++ Assets/_Scripts/interface/soundOnHover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//plays the sound in the audiosource
+public class soundOnHover : MonoBehaviour {
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// plays the sound in the audio source.
+    /// </summary>
+	public void PlaySound()
+    {
+        audioSource.Play();
+    }
+}
