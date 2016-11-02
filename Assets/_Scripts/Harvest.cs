e97d28fcb530939b59a00e61690444a3b2ef8f3d
NabilCherigui <mrtopgamer@hotmail.com>
2016-11-02 14:35:32 +0100

    336 - 3

---
+++ Assets/_Scripts/Harvest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Harvest : MonoBehaviour {
+   
+	Vector3 _cursorWorld; // Will be changed when the input handler is ready
+    Collider2D Collisionbox;
+    
+    Ray ray;
+    
+	// Update is called once per frame
+	void Update () {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+
+<<<<<<< HEAD
+        //if (Collisionbox.)
+        {
+=======
+    }
+    private void PlayAnimation()
+    {
+>>>>>>> origin/master
+
+    }
+    private Item[] HarvestLand(GameObject land)
+    {
+        return land.GetComponent<land>().RemovePlant();
+    }
+}
