a014e24bb13f02df5ff64205f33ec8fd74ab6beb
NabilCherigui <mrtopgamer@hotmail.com>
2016-11-02 16:15:58 +0100

    Boer animatie controls

--- Assets/_Scripts/PlayerAnim.cs
+++ Assets/_Scripts/PlayerAnim.cs
@@ -6,19 +6,24 @@ public class PlayerAnim : MonoBehaviour {
 	Animator anim;
 
 	void Start(){
+		//Verandert de variabele "anim" naar de component animator
 		anim = GetComponent<Animator> ();
 	}
 
 	void Update(){
+		//Terwijl de knop "O" wordt ingerukt, wordt de oogst animatie afgespeeldt
 		if (Input.GetKeyDown (KeyCode.O)) {
 			anim.SetInteger ("State", 1);
 		}
+		//Als de knop "O" wordt losgelaten, gaat de animatie terug naar de idle
 		if (Input.GetKeyUp (KeyCode.O)) {
 			anim.SetInteger ("State", 0);
 		}
+		//Terwijl de knop "Z" wordt ingerukt, wordt de zaai animatie afgespeeldt
 		if (Input.GetKeyDown (KeyCode.Z)) {
 			anim.SetInteger ("State", 2);
 		}
+		//Als de knop "Z" wordt losgelaten, gaat de animatie terug naar de idle
 		if (Input.GetKeyUp (KeyCode.Z)) {
 			anim.SetInteger ("State", 0);
 		}
