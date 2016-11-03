bfd9c96319b1151da3da84cfa4041f419bb1ef84
CoenSteuns <c.steuns@gmail.com>
2016-11-03 09:58:51 +0100

    pauze menu

---
+++ Assets/_Scripts/interface/pauzeGame.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pauzeGame : MonoBehaviour {
+
+    [SerializeField] private GameObject menu;
+    [SerializeField] private KeyCode pausekey;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(pausekey))
+        {
+            if (isPaused)
+            {
+                resumeGame();
+            }
+            else
+            {
+                pausegame();
+            }
+        }
+    }
+
+    public void pausegame()
+    {
+        Time.timeScale = 0;
+        isPaused = true;
+        menu.SetActive(true);
+    }
+
+    public void resumeGame()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        menu.SetActive(false);
+    }
+}
